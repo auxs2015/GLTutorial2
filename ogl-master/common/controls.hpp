bdacdb90fbdd664f268900bdfa89b3db36238e0b
auxs2015 <1058244797@qq.com>
2019-12-06 14:58:08 +0800

    first commit

---
+++ ogl-master/common/controls.hpp
@@ -0,0 +1,9 @@
+#ifndef CONTROLS_HPP
+#define CONTROLS_HPP
+
+void computeMatricesFromInputs(glm::vec3 center);
+void computeMatricesFromInputs();
+glm::mat4 getViewMatrix();
+glm::mat4 getProjectionMatrix();
+
+#endif
